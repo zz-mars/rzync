51734179f2153e0de720c829dafc718cfcb40261
zz chen <grantchen88@gmail.com>
2013-12-31 15:11:24 +0800

    add time

--- test/log_analysis.sh
+++ test/log_analysis.sh
@@ -8,7 +8,14 @@ fi
 LOG_FILE=$1
 
 cat "$LOG_FILE" | \
-		awk 'BEGIN{ts=0;tfsz=0;}	\
-		{tid=tid+$1+$2;tfsz=tfsz+$3}		\
-		END{print "total_interacter_bytes : ",tid,"\ntotal_filesz : ",tfsz,"\ncompression_ratio : ",100.0*(tfsz-tid)/tfsz,"%"}'
+		awk 'BEGIN{t=0;ts=0;tfsz=0;}{
+			if (NF==3) {
+				tid=tid+$1+$2;
+				tfsz=tfsz+$3;
+			} else if (NF==1) {
+				t = $1;
+			}
+		}END{
+			print "time elapsed : ",t,"s\ntotal_interacter_bytes : ",tid,"bytes\ntotal_filesz : ",tfsz,"bytes\ncompression_ratio : ",100.0*(tfsz-tid)/tfsz,"%"
+		}'
 
