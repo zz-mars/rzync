0c055c878a6c7d56ea0ccf16648068d56a96a50c
zz chen <grantchen88@gmail.com>
2014-01-01 17:58:00 +0800

    roll

--- rolling_test.c
+++ rolling_test.c
@@ -22,8 +22,24 @@ int main(int argc, char* argv[])
 	unsigned long long size = stt.st_size;
 
 	unsigned long long bytes_processed = 0;
-	unsigned int total_in_buf = 0;
-	unsigned int in_buf_not_processed = 0;
+	unsigned int buf_len = 0;
+	unsigned int buf_off = 0;
+
+	int fd = open(file,O_RDONLY);
+	if(fd < 0) {
+		perror("open file");
+		return 1;
+	}
+
+	int n = read(fd,buf,BUFSIZ);
+	if(n < 0) {
+		perror("read file");
+		return 1;
+	}
+
+	buf_len = n;
+	if(buf_len < TEST_BLK_SZ) {
+	}
 
 	while(bytes_processed != size) {
 	}
