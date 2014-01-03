200c09158759b6a12c102cc8961f54b4b029e4e3
zz chen <grantchen88@gmail.com>
2014-01-03 16:46:26 +0800

    cdc

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 CC_FLAGS = -g -o
 LINK_LIBS = -levent
-BINS = rzsrc rzdst 
+BINS = rzsrc rzdst cdc
 .PHONY : all
 all : rzdst rzsrc
 rzsrc : rzsrc.o adler32.o md5.o
 	gcc $? $(CC_FLAGS) $@ 
 rzdst : rzdst.o adler32.o md5.o
 	gcc $? $(CC_FLAGS) $@ $(LINK_LIBS)
+cdc : cdc.o md5.o adler32.o
+	gcc $? $(CC_FLAGS) $@
 
 .PHONY : clean
 clean :
--- cdc.c
+++ cdc.c
@@ -1,41 +1,140 @@
-#include "cdc.h"
-#include "adler32.h"
-#include "md5.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <assert.h>
+#include "md5.h"
+#include "adler32.h"
 
-int file_cdc(char* filename,char* cdc_filename,int* block_nr_p)
+#define BLOCK_SZ		(1<<10)
+#define BLOCK_MIN_SZ	(1<<9)
+#define BLOCK_MAX_SZ	(1<<11)
+#define BLOCK_WIN_SZ	48
+
+#define MIN_BLK_SZ	(BLOCK_MIN_SZ-BLOCK_WIN_SZ)
+
+#define BUF_MAX_SZ		(1<<15)
+
+#define CHUNK_CDC_D		BLOCK_SZ
+#define CHUNK_CDC_R		13
+
+#define CDC_MD5_LEN		32
+
+/* content-defined chunking */
+static int file_chunk_cdc(int fd_src)
 {
-	unsigned char buf[BUF_MAX_SZ];
-	unsigned char block_buf[BLOCK_MAX_SZ];
-	unsigned char wbuf[BLOCK_WIN_SZ];
-
+	char buf[BUF_MAX_SZ] = {0};
+	char block_buf[BLOCK_MAX_SZ] = {0};
+	char win_buf[BLOCK_WIN_SZ + 1] = {0};
+	unsigned char md5_checksum[CDC_MD5_LEN+1] = {0};
 	unsigned int bpos = 0;
 	unsigned int rwsize = 0;
-
-	unsigned int block_sz = 0,old_block_sz = 0;
-
+	unsigned int exp_rwsize = BUF_MAX_SZ;
+	unsigned int head, tail;
+	unsigned int block_sz = 0, old_block_sz = 0;
+	unsigned int hkey = 0;
 	unsigned long long offset = 0;
 
-	unsigned int block_nr = 0;	// how many blocks
+	while(rwsize = read(fd_src, buf + bpos, exp_rwsize)) {
+		/* last chunk */
+		if ((rwsize + bpos + block_sz) < BLOCK_MIN_SZ)
+			break;
+
+		head = 0;
+		tail = bpos + rwsize;
+		/* avoid unnecessary computation and comparsion */
+		if (block_sz < (BLOCK_MIN_SZ - BLOCK_WIN_SZ)) {
+			old_block_sz = block_sz;
+			block_sz = ((block_sz + tail - head) > (BLOCK_MIN_SZ - BLOCK_WIN_SZ)) ?
+				BLOCK_MIN_SZ - BLOCK_WIN_SZ : block_sz + tail -head;
+			memcpy(block_buf + old_block_sz, buf + head, block_sz - old_block_sz);
+			head += (block_sz - old_block_sz);
+		}
+
+		while ((head + BLOCK_WIN_SZ) <= tail) {
+			memcpy(win_buf, buf + head, BLOCK_WIN_SZ);
+			hkey = (block_sz == (BLOCK_MIN_SZ - BLOCK_WIN_SZ)) ? adler32_checksum(win_buf, BLOCK_WIN_SZ) :
+				adler32_rolling_checksum(hkey, BLOCK_WIN_SZ, buf[head-1], buf[head+BLOCK_WIN_SZ-1]);
+
+			/* get a normal chunk, write block info to chunk file */
+			if ((hkey % BLOCK_SZ) == CHUNK_CDC_R) {
+				memcpy(block_buf + block_sz, buf + head, BLOCK_WIN_SZ);
+				head += BLOCK_WIN_SZ;
+				block_sz += BLOCK_WIN_SZ;
+				if (block_sz >= BLOCK_MIN_SZ) {
+					md5s_of_str(block_buf, block_sz, md5_checksum);
+					md5_checksum[CDC_MD5_LEN] = '\0';
+					printf("%s\n",md5_checksum);
+					offset += block_sz;
+					block_sz = 0;
+				}
+			} else {
+				block_buf[block_sz++] = buf[head++];
+				/* get an abnormal chunk, write block info to chunk file */
+				if (block_sz >= BLOCK_MAX_SZ) {
+					md5s_of_str(block_buf, block_sz, md5_checksum);
+					md5_checksum[CDC_MD5_LEN] = '\0';
+					printf("%s\n",md5_checksum);
+					offset += block_sz;
+					block_sz = 0;
+				}
+			}
+
+			/* avoid unnecessary computation and comparsion */
+			if (block_sz == 0) {
+				block_sz = ((tail - head) > (BLOCK_MIN_SZ - BLOCK_WIN_SZ)) ?
+					BLOCK_MIN_SZ - BLOCK_WIN_SZ : tail - head;
+				memcpy(block_buf, buf + head, block_sz);
+				head = ((tail - head) > (BLOCK_MIN_SZ - BLOCK_WIN_SZ)) ?
+					head + (BLOCK_MIN_SZ - BLOCK_WIN_SZ) : tail;
+			}
+		}
+
+		/* read expected data from file to full up buf */
+		bpos = tail - head;
+		exp_rwsize = BUF_MAX_SZ - bpos;
+		memmove(buf, buf + head, bpos);
+	}
+
+	if (rwsize == -1)
+		return -1;
+	
+	/* process last block */
+	unsigned int last_block_sz = ((rwsize + bpos + block_sz) >= 0) ? rwsize + bpos + block_sz : 0;
+	char last_block_buf[BLOCK_MAX_SZ] = {0};
+	if (last_block_sz > 0) {
+		memcpy(last_block_buf, block_buf, block_sz);
+		memcpy(last_block_buf + block_sz, buf, rwsize + bpos);
+		md5s_of_str(last_block_buf, last_block_sz, md5_checksum);
+		md5_checksum[CDC_MD5_LEN] = '\0';
+		printf("%s\n",md5_checksum);
+	}
+
+	return 0;
+}
+
+enum {
+	CDC_OK = 0,
+	CDC_ERR
+};
+
+int file_cdc(int fd)
+{
+	unsigned char block_buf[BLOCK_MAX_SZ];
+	unsigned char wbuf[BLOCK_WIN_SZ];
+	unsigned char md5[CDC_MD5_LEN+1];
 
 	int ret = CDC_ERR;
 
-	int fd = open(filename,O_RDONLY);
-	if(fd < 0) {
-		perror("open file");
-		goto rt;
-	}
+	unsigned int block_sz = 0;
+	unsigned int block_nr = 0;	// how many blocks
+	unsigned long long offset = 0;
 
-	int cdcfd = open(cdc_filename,O_TRUNC | O_CREAT | O_WRONLY,0660);
-	if(cdcfd < 0) {
-		perror("open cdc file");
-		goto close_fd;
-	}
-
+	unsigned char buf[BUF_MAX_SZ];
 	unsigned int exp_rwsize = BUF_MAX_SZ;
-	while(rwsize = read(fd,buf+bpos,exp_rwsize)) {
+	unsigned int bpos = 0;
+	unsigned int rwsize = 0;
+	unsigned long long offset = 0;
+	while((rwsize = read(fd,buf+bpos,exp_rwsize)) > 0) {
 		/* last block */
 		if((block_sz + bpos + rwsize) < BLOCK_MIN_SZ) {
 			break;
@@ -53,7 +152,6 @@ int file_cdc(char* filename,char* cdc_filename,int* block_nr_p)
 		}
 		/* sliding! */
 		unsigned int hkey = 0;
-		unsigned char md5[CDC_MD5_LEN+1];
 		while((head + BLOCK_WIN_SZ) <= tail) {
 			memcpy(wbuf,buf+head,BLOCK_WIN_SZ);
 			hkey = (block_sz == MIN_BLK_SZ) ? adler32_checksum(wbuf,BLOCK_WIN_SZ) : 
@@ -66,6 +164,7 @@ int file_cdc(char* filename,char* cdc_filename,int* block_nr_p)
 					goto blk_found;
 				}
 			} else {
+				/* one byte forward */
 				block_buf[block_sz++] = buf[head++];
 				assert(block_sz <= BLOCK_MAX_SZ);
 				if(block_sz == BLOCK_MAX_SZ) {
@@ -76,12 +175,9 @@ int file_cdc(char* filename,char* cdc_filename,int* block_nr_p)
 blk_found:
 			block_nr++;
 			md5s_of_str(block_buf,block_sz,md5);
-			md5[CDC_MD5_LEN] = '\n';
-			/* write to file */
-			if(write(cdcfd,md5,CDC_MD5_LEN+1) != (CDC_MD5_LEN+1)) {
-				perror("write");
-				goto close_fd;
-			}
+			md5[CDC_MD5_LEN] = '\0';
+			printf("%s\n",md5);
+
 			offset += block_sz;
 			unsigned int bytes_in_buf = tail - head;
 			block_sz = (bytes_in_buf > MIN_BLK_SZ) ?
@@ -90,6 +186,7 @@ blk_found:
 			head += block_sz;
 		}
 		/* try to read more */
+		assert(tail >= head);
 		bpos = tail - head;
 		exp_rwsize = BUF_MAX_SZ  - bpos;
 		memmove(buf,buf+head,bpos);
@@ -101,16 +198,11 @@ blk_found:
 	if(last_blk_sz > 0) {
 		block_nr++;
 		memcpy(block_buf+block_sz,buf,bpos+rwsize);
-		unsigned char md5[CDC_MD5_LEN+1];
 		md5s_of_str(block_buf,last_blk_sz,md5);
-		md5[CDC_MD5_LEN] = '\n';
-		if(write(cdcfd,md5,CDC_MD5_LEN+1) != (CDC_MD5_LEN+1)) {
-			perror("write");
-			goto close_fd;
-		}
+		md5[CDC_MD5_LEN] = '\0';
+		printf("%s\n",md5);
 	}
 
-	*block_nr_p = block_nr;
 	ret = CDC_OK;
 close_fd:
 	close(fd);
@@ -120,19 +212,22 @@ rt:
 
 int main(int argc,char* argv[])
 {
-	if(argc != 3) {
-		fprintf(stderr,"Usage : ./cdc <src_file> <cdc_file>\n");
+	if(argc != 2) {
+		fprintf(stderr,"Usage : ./cdc <src_file>\n");
 		return 1;
 	}
 	char* filename = argv[1];
-	char* cdc_file = argv[2];
 
-	int block_nr;
-
-	if(file_cdc(filename, cdc_file, &block_nr) != CDC_OK) {
-		fprintf(stderr,"CDC ERR -- %s\n",filename);
+	int fd = open(filename,O_RDONLY);
+	if(fd < 0) {
+		perror("open");
 		return 1;
 	}
+
+	file_chunk_cdc(fd);
+
+	close(fd);
+
 	return 0;
 }
 
